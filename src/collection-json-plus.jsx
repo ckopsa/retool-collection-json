f08ecab3f83aa0133bc16a02309c615c9701c109
Colton Kopsa <colton@kopsa.dev>
2023-01-20 13:04:06 -0800

    Fix disabled controls

--- src/collection-json-plus.jsx
+++ src/collection-json-plus.jsx
@@ -119,7 +119,8 @@ const MyCustomComponent = ({triggerQuery, model, modelUpdate}) => {
               case 'select':
                 return <div hidden={datum.display === false} className="mb-3">
                   <label className="form-label me-2" htmlFor={datum.name}>{datum.prompt}: </label>
-                  <select id={datum.name} name={datum.name} defaultValue={datum.value} className="form-select text-dark"
+                  <select id={datum.name} name={datum.name} defaultValue={datum.value}
+                          className={!!datum.readOnly ? "form-select" : "form-select text-dark"}
                           required={!!datum.required} disabled={!!datum.readOnly}>
                     <option></option>
                     {datum.suggest.map(sug => {
@@ -138,13 +139,17 @@ const MyCustomComponent = ({triggerQuery, model, modelUpdate}) => {
                 return <div hidden={datum.display === false} className="mb-3">
                   <label className="form-label me-2" htmlFor={datum.name}>{datum.prompt}: </label>
                   <input type="date" id={datum.name} name={datum.name} defaultValue={datum.value || ''}
-                         required={!!datum.required} className="form-control text-dark" disabled={!!datum.readOnly}/>
+                         required={!!datum.required}
+                         className={!!datum.readOnly ? "form-select" : "form-select text-dark"}
+                         disabled={!!datum.readOnly}/>
                 </div>
               default:
                 return <div hidden={datum.display === false} className="mb-3">
                   <label className="form-label me-2" htmlFor={datum.name}>{datum.prompt}: </label>
                   <input type="text" id={datum.name} name={datum.name} defaultValue={datum.value || ''}
-                         required={!!datum.required} className="form-control text-dark" disabled={!!datum.readOnly}/>
+                         required={!!datum.required}
+                         className={!!datum.readOnly ? "form-select" : "form-select text-dark"}
+                         disabled={!!datum.readOnly}/>
                 </div>
             }
 
@@ -159,51 +164,49 @@ const MyCustomComponent = ({triggerQuery, model, modelUpdate}) => {
   };
 
   return (<div className="h-100 text-body bg-body" data-bs-theme="dark">
-    {fullMode &&
-      <nav className="navbar navbar-expand-lg rounded">
-        <div className="container-fluid">
-          <button className="btn" onClick={goBack} hidden={historyStackIsEmpty()}>
-            Back
-          </button>
+    {fullMode && <nav className="navbar navbar-expand-lg rounded">
+      <div className="container-fluid">
+        <button className="btn" onClick={goBack} hidden={historyStackIsEmpty()}>
+          Back
+        </button>
 
-          <div className="collapse navbar-collapse justify-content-md-center">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active"
-                   href={model.cj.collection.href}
-                   onClick={(e) => httpGet(e, model.cj.collection.href)}>
-                  {model.cj.collection.title}
-                </a>
-              </li>
-              {model.cj.collection.links.filter(link => link.rel.includes('local'))
-                .map(link => <li className="nav-item" key={link.name}>
-                  <a className="nav-link" href={link.href}
-                     onClick={(e) => httpGet(e, link.href)}>{link.prompt}</a>
-                </li>)}
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
-                   onClick={() => setShowApps(!showApps)}
-                >
-                  Apps
-                </a>
-                <ul className={showApps ? "dropdown-menu show" : "dropdown-menu"}>
-                  {model.cj.collection.links.filter(link => !link.rel.includes('local')).map(l => {
-                    return <li key={l.name}>
-                      <a className="dropdown-item" href={l.href}
-                         onClick={(e) => httpGet(e, l.href)}>{l.prompt}</a>
-                    </li>
-                  })}
-                </ul>
-              </li>
-            </ul>
-          </div>
-
-          <button className="btn" onClick={goForward} hidden={futureStackIsEmpty()}>
-            Forward
-          </button>
+        <div className="collapse navbar-collapse justify-content-md-center">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active"
+                 href={model.cj.collection.href}
+                 onClick={(e) => httpGet(e, model.cj.collection.href)}>
+                {model.cj.collection.title}
+              </a>
+            </li>
+            {model.cj.collection.links.filter(link => link.rel.includes('local'))
+              .map(link => <li className="nav-item" key={link.name}>
+                <a className="nav-link" href={link.href}
+                   onClick={(e) => httpGet(e, link.href)}>{link.prompt}</a>
+              </li>)}
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                 onClick={() => setShowApps(!showApps)}
+              >
+                Apps
+              </a>
+              <ul className={showApps ? "dropdown-menu show" : "dropdown-menu"}>
+                {model.cj.collection.links.filter(link => !link.rel.includes('local')).map(l => {
+                  return <li key={l.name}>
+                    <a className="dropdown-item" href={l.href}
+                       onClick={(e) => httpGet(e, l.href)}>{l.prompt}</a>
+                  </li>
+                })}
+              </ul>
+            </li>
+          </ul>
         </div>
-      </nav>
-    }
+
+        <button className="btn" onClick={goForward} hidden={futureStackIsEmpty()}>
+          Forward
+        </button>
+      </div>
+    </nav>}
     <div className="progress" hidden={!loading}>
       <div className="progress-bar progress-bar-striped progress-bar-animated w-100" role="progressbar"
            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"/>
@@ -217,32 +220,28 @@ const MyCustomComponent = ({triggerQuery, model, modelUpdate}) => {
       </div>}
     </div>
     <div className="row ps-4 w-100 d-flex justify-content-center">
-      {items.length !== 0 &&
-        <div className="col-6">
-          {items.map(it => <div className="card p-3 mb-3">
-            <ul className="list-group list-group-flush">
-              {it.data.map((itData, i) => <li
-                name={itData.name}
-                className="list-group-item d-flex justify-content-between">
-                {itData.prompt}: <span>{itData.value}</span>
-              </li>)}
-            </ul>
-            <div className="card-body">
-              {fullMode && it.links && it.links.map((itLink, i) => <a
-                href={itLink.href} className="card-link btn btn-primary"
-                onClick={(e) => httpGet(e, itLink.href)}>
-                {itLink.prompt}
-              </a>)}
-            </div>
-          </div>)}
-        </div>
-      }
-      {(queries.length !== 0 || commands.length !== 0) &&
-        <div className="col-6">
-          {queries.map(query => queryToComponent(query, httpQuery))}
-          {commands.map(command => queryToComponent(command, httpPost))}
-        </div>
-      }
+      {items.length !== 0 && <div className="col-6">
+        {items.map(it => <div className="card p-3 mb-3">
+          <ul className="list-group list-group-flush">
+            {it.data.map((itData, i) => <li
+              name={itData.name}
+              className="list-group-item d-flex justify-content-between">
+              {itData.prompt}: <span>{itData.value}</span>
+            </li>)}
+          </ul>
+          <div className="card-body">
+            {fullMode && it.links && it.links.map((itLink, i) => <a
+              href={itLink.href} className="card-link btn btn-primary"
+              onClick={(e) => httpGet(e, itLink.href)}>
+              {itLink.prompt}
+            </a>)}
+          </div>
+        </div>)}
+      </div>}
+      {(queries.length !== 0 || commands.length !== 0) && <div className="col-6">
+        {queries.map(query => queryToComponent(query, httpQuery))}
+        {commands.map(command => queryToComponent(command, httpPost))}
+      </div>}
     </div>
   </div>)
 }
